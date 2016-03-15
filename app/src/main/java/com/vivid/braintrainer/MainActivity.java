374c0bb07c0e19f3d535111793ff9b3052da2101
NattieVivid <nattie.vivid@gmail.com>
2016-03-15 20:44:48 +0200

    commit

--- app/src/main/java/com/vivid/braintrainer/MainActivity.java
+++ app/src/main/java/com/vivid/braintrainer/MainActivity.java
@@ -4,14 +4,38 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     Button startButton;
 
+    TextView tvTimeLeft;
+    TextView tvScore;
+    TextView tvQuestion;
+
+    Button btnAnswer1;
+    Button btnAnswer2;
+    Button btnAnswer3;
+    Button btnAnswer4;
+
     public void start(View view){
 
         startButton.setVisibility(View.INVISIBLE);
+        show();
+
+    }
+
+    public void show(){
+
+        btnAnswer1.setVisibility(View.VISIBLE);
+        btnAnswer2.setVisibility(View.VISIBLE);
+        btnAnswer3.setVisibility(View.VISIBLE);
+        btnAnswer4.setVisibility(View.VISIBLE);
+
+        tvTimeLeft.setVisibility(View.VISIBLE);
+        tvScore.setVisibility(View.VISIBLE);
+        tvQuestion.setVisibility(View.VISIBLE);
 
     }
 
@@ -22,6 +46,16 @@ public class MainActivity extends Activity {
 
         startButton = (Button) findViewById(R.id.startButton);
         startButton.setVisibility(View.VISIBLE);
+
+        btnAnswer1 = (Button) findViewById(R.id.btnAnswer1);
+        btnAnswer2 = (Button) findViewById(R.id.btnAnswer2);
+        btnAnswer3 = (Button) findViewById(R.id.btnAnswer3);
+        btnAnswer4 = (Button) findViewById(R.id.btnAnswer4);
+
+        tvTimeLeft = (TextView) findViewById(R.id.tvTimeLeft);
+        tvScore = (TextView) findViewById(R.id.tvScore);
+        tvQuestion = (TextView) findViewById(R.id.tvQuestion);
+
 
     }
 }
